e0a669ac4ceb368587dc9dd83c638251726fefa4
Inaboya <momohdavid21@gmail.com>
2023-02-06 15:23:42 +0100

    set up project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="">hello world</div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap');
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Roboto Slab', 'Montserrat', sans-serif;
+}
+
+body {
+  background-color: #cecece;
+}
+</style>
